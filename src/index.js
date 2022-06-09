5b93cdcb280adecaa36c055794f5224806e36077
Muhammad Faruq <muhammadfaruq.tkj1@gmail.com>
2022-06-09 22:04:01 +0700

    tess

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Home } from './Components/Home';
 import reportWebVitals from './reportWebVitals';
 import { Test } from './Test';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Test />
+    <Home />
   </React.StrictMode>
 );
 
