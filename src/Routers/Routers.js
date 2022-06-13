508f10e17542e9a0cadd379d99ae39f587bb075a
Dery <69612416+derynm@users.noreply.github.com>
2022-06-13 21:07:27 +0700

    install redux

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home } from '../Views/Home/Home';
-import { Login } from '../Views/Auth/Login';
-import { Register } from '../Views/Auth/Register';
-import { AddProduct } from '../Views/Product/AddProduct';
-import { EditProduct } from '../Views/Product/EditProduct';
-import { DeleteProduct } from '../Views/Product/DeleteProduct';
+import { Home } from "../Views/Home/Home";
+import { Login } from "../Views/Auth/Login";
+import { Register } from "../Views/Auth/Register";
+import { AddProduct } from "../Views/Product/AddProduct";
+import { EditProduct } from "../Views/Product/EditProduct";
+import { DeleteProduct } from "../Views/Product/DeleteProduct";
 
 export const Routers = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
 
-            {/* Auth */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+      {/* Auth */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-            {/* Product */}
-            <Route path="/add-product" element={<AddProduct />} />
-            <Route path="/edit-product" element={<EditProduct />} />
-            <Route path="/delete-product" element={<DeleteProduct />} />
-        </Routes>
-    )
+      {/* Product */}
+      <Route path="/add-product" element={<AddProduct />} />
+      <Route path="/edit-product" element={<EditProduct />} />
+      <Route path="/delete-product" element={<DeleteProduct />} />
+    </Routes>
+  );
 };
--- src/Views/Layout.js
+++ src/Views/Layout.js
@@ -4,22 +4,22 @@ import { BrowserRouter } from "react-router-dom";
 import { Routers } from "../Routers/Routers";
 
 export const Layout = () => {
-    let kondisi = true;
-    return (
-        <div>
-            {kondisi ? (
-                <>
-                    <BrowserRouter>
-                        <Routers />
-                    </BrowserRouter>
-                </>
-            ) : (
-                <>
-                    <BrowserRouter>
-                        <Routers />
-                    </BrowserRouter>
-                </>
-            )}
-        </div>
-    );
+  let kondisi = true;
+  return (
+    <div>
+      {kondisi ? (
+        <>
+          <BrowserRouter>
+            <Routers />
+          </BrowserRouter>
+        </>
+      ) : (
+        <>
+          <BrowserRouter>
+            <Routers />
+          </BrowserRouter>
+        </>
+      )}
+    </div>
+  );
 };
