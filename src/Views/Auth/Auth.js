171bc80ef86eb44578e3da483bcc5861d241374b
Dery <69612416+derynm@users.noreply.github.com>
2022-06-15 21:08:54 +0700

    fix css

--- src/Views/Auth/Auth.js
+++ src/Views/Auth/Auth.js
@@ -8,34 +8,82 @@ export const Auth = () => {
   //state untuk ganti input type password
   const [PasswordShow, setPasswordShow] = useState(false);
 
+  const [RegisterState, setRegisterState] = useState({
+    nama: null,
+    email: null,
+    password: null,
+  });
+  const [LoginState, setLoginState] = useState({
+    email: null,
+    password: null,
+  });
+
   //function untuk ganti dari login ke register dan sebaliknya
   const handlePageType = () => {
     setPageType(!PageType);
+    setLoginState({
+      email: "",
+      password: "",
+    });
+    setRegisterState({
+      nama: "",
+      email: "",
+      password: "",
+    })
+  };
+
+  const handleState = (e, type, prop) => {
+    type === "register"
+      ? setRegisterState({
+          ...RegisterState,
+          [prop]: e.target.value,
+        })
+      : setLoginState({
+          ...LoginState,
+          [prop]: e.target.value,
+        });
   };
 
   //function berisi form login
   const formLogin = () => {
     return (
       <form>
+      {console.log(LoginState)}
         <h3>Masuk</h3>
         <div className="mb-3">
-          <label className="form-label">
-            Email
-          </label>
-          <input type="email" className="form-control" id="email-login" />
+          <label className="form-label">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email-login"
+            onChange={(e) => {
+              handleState(e, "login", "email");
+            }}
+            value={LoginState.email}
+          />
         </div>
         <div className="mb-1">
-          <label className="form-label">
-            Password
-          </label>
-          <input type={PasswordShow? "text":"password"} className="form-control" id="password-login" />
+          <label className="form-label">Password</label>
+          <input
+            type={PasswordShow ? "text" : "password"}
+            className="form-control"
+            id="password-login"
+            onChange={(e) => {
+              handleState(e, "login", "password");
+            }}
+          />
         </div>
 
         <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="showPassword" onChange={()=>{setPasswordShow(!PasswordShow)}}/>
-          <label className="form-check-label">
-            Tampilkan Password
-          </label>
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="showPassword"
+            onChange={() => {
+              setPasswordShow(!PasswordShow);
+            }}
+          />
+          <label className="form-check-label">Tampilkan Password</label>
         </div>
 
         <button type="submit" className="button-auth mb-5">
@@ -62,26 +110,49 @@ export const Auth = () => {
       <form>
         <h3>Daftar</h3>
         <div className="mb-3">
-          <label className="form-label">
-            Nama
-          </label>
-          <input type="text" className="form-control" id="nama-register" />
+          <label className="form-label">Nama</label>
+          <input
+            type="text"
+            className="form-control"
+            id="nama-register"
+            onChange={(e) => {
+              handleState(e, "register", "nama");
+            }}
+          />
         </div>
         <div className="mb-3">
-          <label className="form-label">
-            Email
-          </label>
-          <input type="email" className="form-control" id="email-register" />
+          <label className="form-label">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email-register"
+            onChange={(e) => {
+              handleState(e, "register", "email");
+            }}
+          />
         </div>
         <div className="mb-3">
-          <label className="form-label">
-            Password
-          </label>
+          <label className="form-label">Password</label>
           <input
             type="password"
             className="form-control"
             id="password-register"
+            onChange={(e) => {
+              handleState(e, "register", "password");
+            }}
           />
+        </div>
+
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="showPassword"
+            onChange={() => {
+              setPasswordShow(!PasswordShow);
+            }}
+          />
+          <label className="form-check-label">Tampilkan Password</label>
         </div>
 
         <button type="submit" className="button-auth mb-5">
