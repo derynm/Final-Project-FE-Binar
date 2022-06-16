61379af3aaf5947aea38aaec9e76177c907a8526
Dery <69612416+derynm@users.noreply.github.com>
2022-06-16 20:25:38 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Home } from './Components/Home';
 import reportWebVitals from './reportWebVitals';
-// import { Test } from './Test';
 import { Layout } from './Views/Layout';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./Assets/Css/Global.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
