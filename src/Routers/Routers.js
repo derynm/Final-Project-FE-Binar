2ed498b6a5a04137b232433be0015da6db2b87c5
Dery <69612416+derynm@users.noreply.github.com>
2022-06-28 20:28:08 +0700

    add page

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../Views/Home/Home";
 import { AddProduct } from "../Views/Product/AddProduct";
-import { EditProduct } from "../Views/Product/EditProduct";
-import { DeleteProduct } from "../Views/Product/DeleteProduct";
 import { Auth } from "../Views/Auth/Auth";
 import { Login } from "../Views/Auth/Login";
 import { Register } from "../Views/Auth/Register";
 import { HomeSlider } from "../Assets/Components/HomeSlider/HomeSlider";
 import { CardHomePage } from "../Assets/Components/CardHomePage/CardHomePage";
+import { DetailProduct } from "../Views/Product/DetailProduct";
 
 export const Routers = () => {
   return (
@@ -25,8 +24,7 @@ export const Routers = () => {
 
       {/* Product */}
       <Route path="/add-product" element={<AddProduct />} />
-      <Route path="/edit-product" element={<EditProduct />} />
-      <Route path="/delete-product" element={<DeleteProduct />} />
+      <Route path="/detail-product" element={<DetailProduct />} />
     </Routes>
   );
 };
