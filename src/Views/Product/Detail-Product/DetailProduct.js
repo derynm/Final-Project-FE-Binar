613c281a1e9803857efec27e95eb1440f915cb62
Dery <69612416+derynm@users.noreply.github.com>
2022-06-28 21:41:38 +0700

    layouting

--- src/Views/Product/Detail-Product/DetailProduct.js
+++ src/Views/Product/Detail-Product/DetailProduct.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React from "react";
+import gambar from "../../../Assets/Img/airmax.jpg";
+import "./DetailProduct.css";
 
 export const DetailProduct = () => {
   return (
-    <div>DetailProduct</div>
-  )
-}
+    <div className="detail-product-main">
+      <div className="container-sm">
+        <div className="row">
+          <div
+            className="col col-lg-8 col-sm-12 col-12"
+          >
+            <div className="row">
+              <div
+                className="col-12 d-flex justify-content-center"
+              >
+                <img
+                  src={gambar}
+                  alt="product"
+                  className="detail-product-image"
+                />
+              </div>
+              <div
+                className="col-12 d-flex justify-content-center"
+              >
+                <div className="detail-product-description">
+                  <h5>Deskripsi</h5>
+                  <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum."
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum."
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="col col-lg-4 col-sm-12 col-12"
+            style={{ backgroundColor: "red" }}
+          >
+            a
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
