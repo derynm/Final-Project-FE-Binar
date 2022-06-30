a9280fc05a34ebc7272ddca2e2d73b14eff41553
Dery <69612416+derynm@users.noreply.github.com>
2022-06-30 19:34:23 +0700

    offer component

--- src/Assets/Components/Offer/OfferComponents.js
+++ src/Assets/Components/Offer/OfferComponents.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import { CardSeller } from '../Card/CardSeller/CardSeller'
+import React from "react";
+import { CardSellerInfo } from "../Card/CardSeller/CardSeller";
+import produk from "../../Img/airmax.jpg";
+import "./OfferComponents.css";
 
 export const OfferComponents = () => {
   return (
-    <div className='offer-main'>
-        <CardSeller/>
+    <div className="offer-main d-flex">
+      <CardSellerInfo />
+      <p className="offer-title">Daftar Produk yang Ditawar</p>
+      <div className="offer-content">
+        <img className="offer-content-image" src={produk} alt="asd" />
+      </div>
     </div>
-  )
-}
+  );
+};
