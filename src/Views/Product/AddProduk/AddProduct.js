04fff2a1f8d87eb80c909b1fa7f4d4efaafbfe7b
Dery <69612416+derynm@users.noreply.github.com>
2022-07-08 20:55:04 +0700

    add nav on add-product

--- src/Views/Product/AddProduk/AddProduct.js
+++ src/Views/Product/AddProduk/AddProduct.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
+import { NavbarSecond } from '../../../Assets/Components/NavBar/NavbarSecond';
 import './AddProduct.css';
 
 export const AddProduct = () => {
@@ -9,15 +10,25 @@ export const AddProduct = () => {
         hargaProduk: null,
         kategori: null,
         deskripsi: null,
-        fotoProduk: null
+        fotoProduk: null,
+        isFailed: false,
     });
 
     const handleState = (e, prop) => {
         if (prop === "fotoProduk") {
-            setInputProduk({
-                ...inputProduk,
-                [prop]: e.target.files[0],
-            });
+            if (e.target.files[0].size <= 1048576) {
+                setInputProduk({
+                    ...inputProduk,
+                    [prop]: e.target.files[0],
+                    isFailed: true,
+                });
+            }else {
+                alert("Ukuran file terlalu besar")
+                setInputProduk({
+                    ...inputProduk,
+                    isFailed: true,
+                });
+            }
         } else {
             setInputProduk({
                 ...inputProduk,
@@ -28,6 +39,7 @@ export const AddProduct = () => {
 
     return (
         <div>
+        <NavbarSecond page={"Input Produk"}/>
             <div className='container add-produk-main'>
                 <Form>
                     {console.log(inputProduk)}
@@ -51,11 +63,11 @@ export const AddProduct = () => {
                             onChange={(e) => {
                                 handleState(e, "kategori")
                             }} >
-                            <option value="adidas">Adidas</option>
-                            <option value="jordan">Jordan</option>
-                            <option value="puma">Puma</option>
-                            <option value="nike">Nike</option>
-                            <option value="vans">Vans</option>
+                            <option value="3">Adidas</option>
+                            <option value="5">Jordan</option>
+                            <option value="4">Puma</option>
+                            <option value="1">Nike</option>
+                            <option value="6">Vans</option>
                         </Form.Select>
                     </Form.Group>
                     <Form.Group className="mb-4">
@@ -67,7 +79,7 @@ export const AddProduct = () => {
                     </Form.Group>
                     <Form.Group className="mb-4">
                         <Form.Label>Foto Porduk</Form.Label>
-                        <Form.Control type="file" id="foto_produk" name="foto_produk"
+                        <Form.Control type="file" id="foto_produk" name="foto_produk" accept="image/*"
                             onChange={(e) => {
                                 handleState(e, "fotoProduk")
                             }} />
